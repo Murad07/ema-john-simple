31ac517c9f88643e4b4013732c95fc9847e5aae1
Murad Hossain <sliverboymurad@gmail.com>
2020-08-30 14:28:24 +0600

    react routing and topics til daynamic path set path param

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = () => {
+  return <div>The days not found.</div>;
+};
+
+export default NotFound;
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 const Product = (props) => {
-  const { img, name, seller, price, stock } = props.product;
+  const { img, name, seller, price, stock, key } = props.product;
   return (
     <div className='product'>
       <div>
         <img src={img} alt='' />
       </div>
       <div>
-        <h4 className='product-name'>{name}</h4>
+        <h4 className='product-name'>
+          <Link to={'/product/' + key}>{name}</Link>
+        </h4>
 
         <p>
           <small>by: {seller}</small>
